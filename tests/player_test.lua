5ed5ac36232a2220fb9ea0d09c5c7e94f0fef1ff
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-08-19 19:44:39 -0400

    removed some duplication and useless test

--- tests/player_test.lua
+++ tests/player_test.lua
@@ -2,24 +2,21 @@ module(..., package.seeall)
 
 require "player"
 
-function test_player()
+function setup()
    player = Player()
 end
 
 function test_player_has_sprite()
-   player = Player()
    assert_table(player.sprite)
 end
 
 function test_can_move_right()
-   player = Player()
    player:moveRight()
    local x = player.sprite.x
    assert_gt(160, x)
 end
 
 function test_can_move_left()
-   player = Player()
    player:moveLeft()
    local x = player.sprite.x
    assert_lt(160, x)
