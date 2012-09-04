2d58e8603f66354f4be9cd3939cc33ed7d9781ac
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-03 22:07:37 -0400

    fixed a bug with the water cooler

--- water_cooler.lua
+++ water_cooler.lua
@@ -3,6 +3,8 @@ require 'game_sprite'
 
 WaterCooler = class(GameSprite)
 
-function WaterCooler:init()
+function WaterCooler:init(x)
    self.sprite = display.newImage("images/obstacles/cooler.png")
+   self:setX(x)
+   self:setY(0)
 end
